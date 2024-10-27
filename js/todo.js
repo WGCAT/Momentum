89ab0b91266db1f21b7c5b4301c4ebca20a3a85a
WGCAT <seed9878@naver.com>
2024-10-28 06:02:47 +0900

    Todolist삭제하기

--- js/todo.js
+++ js/todo.js
@@ -3,17 +3,25 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+    // 어떤 버튼을 눌렀는지 판별
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 // newTodo값 생성
 function paintToDo(newTodo) {
     // li값 생성
     const li = document.createElement("li");
     // span값 생성
     const span = document.createElement("span");
-    // li에 span을 넣음
-    li.appendChild(span);
     // span에 텍스트를 넣음
     span.innerText = newTodo;
-    // list에 li를 넣음
+    const button = document.createElement("button");
+    button.innerText = "✖️";
+    button.addEventListener("click", deleteToDo)
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 

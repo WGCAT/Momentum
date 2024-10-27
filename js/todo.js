da493ecfc86433c687dff10e470fe2507238d109
WGCAT <seed9878@naver.com>
2024-10-28 06:11:31 +0900

    Todolist저장하기

--- js/todo.js
+++ js/todo.js
@@ -3,11 +3,20 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+// ToDo저장
+function saveToDos() {
+    // toDos 배열을 localStorage에 집어넣는다
+    localStorage.setItem("todos", JSON.stringify(toDos));
+};
+
+// ToDo삭제
 function deleteToDo(event) {
     // 어떤 버튼을 눌렀는지 판별
     const li = event.target.parentElement;
     li.remove();
-}
+};
 
 // newTodo값 생성
 function paintToDo(newTodo) {
@@ -31,7 +40,9 @@ function handleToDoSubmit(event) {
     const newTodo = toDoInput.value;
     // 엔터 후 input값 삭제
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);

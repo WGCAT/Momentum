f84c7aae122348692d0ffa075ce1644c2637aa92
WGCAT <seed9878@naver.com>
2024-10-28 03:57:33 +0900

    username저장하기

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CLASSNAME = "hidden";
+
+// 유저에게 인사
 function onLoginSubmit(event) {
-    // 브라우저가 기본 동작(새로고침)을 실행하지 못하게 막아줌
+    // 브라우저가 기본 동작(새로고침)을 실행하지 못하게 방지
     event.preventDefault();
+    // 이벤트 발생 시 히든 추가
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    // h1에 텍스트 추가
     const username = loginInput.value;
-    loginForm.classList.add("hidden");
-    console.log(username);
+    // "Hello " + username와 같음
+    greeting.innerText = `Hello ${username}`;
+    // 히든 삭제
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);

d8f8a913842189ebcfbe884a0580201d1c6a2be9
WGCAT <seed9878@naver.com>
2024-10-28 04:13:49 +0900

    username불러오기

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 // 유저에게 인사
 function onLoginSubmit(event) {
@@ -12,10 +13,23 @@ function onLoginSubmit(event) {
     loginForm.classList.add(HIDDEN_CLASSNAME);
     // h1에 텍스트 추가
     const username = loginInput.value;
-    // "Hello " + username와 같음
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+// 유저정보를 받아서 인자로 넣고 히든 삭제하는 함수
+function paintGreetings(username) {
     greeting.innerText = `Hello ${username}`;
-    // 히든 삭제
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+// localStorage에 유저정보가 없다면
+if(savedUsername === null) {
+    // form의 submit를 기다린다
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
